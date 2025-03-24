8cd7d86914c4a0aa527d10eaad847485db82e968
Machariarobert <132147745+Machariarobert@users.noreply.github.com>
2025-03-25 00:22:28 +0300

    Update run-panel.tsx

--- src/components/run-panel/run-panel.tsx
+++ src/components/run-panel/run-panel.tsx
@@ -253,7 +253,7 @@ const RunPanel = observer(() => {
     const { statistics } = transactions;
     const { active_tour, active_tab } = dashboard;
     const { total_payout, total_profit, total_stake, won_contracts, lost_contracts, number_of_runs } = statistics;
-    const { BOT_BUILDER, CHART } = DBOT_TABS;
+    const { BOT_BUILDER, CHART, SIGNALS, ANALYSIS_TOOL } = DBOT_TABS;
 
     React.useEffect(() => {
         onMount();
@@ -296,12 +296,12 @@ const RunPanel = observer(() => {
         />
     );
 
-    const show_run_panel = [BOT_BUILDER, CHART].includes(active_tab) || active_tour;
+    const show_run_panel = [BOT_BUILDER, CHART, ANALYSIS_TOOL, SIGNALS].includes(active_tab) || active_tour;
     if ((!show_run_panel && isDesktop) || active_tour === 'bot_builder') return null;
 
     return (
         <>
-            <div className={!isDesktop && is_drawer_open ? 'run-panel__container--mobile' : 'run-panel'}>
+            <div cassName={!isDesktop && is_drawer_open ? 'run-panel__container--mobile' : 'run-panel'}>
                 <Drawer
                     anchor='right'
                     className={classNames('run-panel', {
