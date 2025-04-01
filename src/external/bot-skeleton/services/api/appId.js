9d02e34578067a3b2361f8051afb6e377ebfeaa5
wallace guthera <machariarobert894@gmail.com>
2025-04-02 01:32:46 +0300

    .

--- src/external/bot-skeleton/services/api/appId.js
+++ src/external/bot-skeleton/services/api/appId.js
@@ -5,10 +5,8 @@ import { getInitialLanguage } from '@deriv-com/translations';
 import APIMiddleware from './api-middleware';
 
 export const generateDerivApiInstance = () => {
-    const cleanedServer = getSocketURL().replace(/[^a-zA-Z0-9.]/g, '');
     const cleanedAppId = getAppId()?.replace?.(/[^a-zA-Z0-9]/g, '') ?? getAppId();
-    const brand = website_name?.toLowerCase() === 'binaryfx' ? 'deriv' : website_name.toLowerCase();
-    const socket_url = `ws://${cleanedServer}/websockets/v3?app_id=${cleanedAppId}&l=${getInitialLanguage()}&brand=${brand}`;
+    const socket_url = `wss://ws.derivws.com/websockets/v3?app_id=${cleanedAppId}`;
     const deriv_socket = new WebSocket(socket_url);
     const deriv_api = new DerivAPIBasic({
         connection: deriv_socket,
